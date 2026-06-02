5e7aef581505393b2a17c32dc69670eb38bbf4aa
Aliaksei <fanikov41@gmail.com>
2026-06-02 20:47:31 +0300

    create file and dir

--- create_dir.py
+++ create_dir.py
@@ -2,16 +2,20 @@ import argparse
 import os
 
 
-def create_folder(folder_name: str) -> str:
+def create_folder_and_file(folder_name: str, file_name: str) -> tuple[str, str]:
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    path = os.path.join(script_dir, folder_name)
-    os.mkdir(path)
-    return path
+    folder_path = os.path.join(script_dir, folder_name)
+    os.mkdir(folder_path)
+    file_path = os.path.join(folder_path, file_name)
+    open(file_path, 'a', encoding='utf-8').close()
+    return folder_path, file_path
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Создать папку рядом со скриптом')
+    parser = argparse.ArgumentParser(description='Создать папку и файл в ней (рядом со скриптом)',)
     parser.add_argument('-fn', '--folder-name', required=True, help='Имя папки')
+    parser.add_argument('-file', '--file-name', required=True, help='Имя файла')
     args = parser.parse_args()
-    created = create_folder(args.folder_name)
-    print(f'Папка создана: {created}')
+    folder_path, file_path = create_folder_and_file(args.folder_name, args.file_name)
+    print(f'Папка создана: {folder_path}')
+    print(f'Файл создан: {file_path}')
