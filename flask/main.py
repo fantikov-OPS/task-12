e9bb30d04412f5781a0389ef6189788b0f20acff
Aliaksei <fanikov41@gmail.com>
2026-06-11 18:39:59 +0300

    add datetime now

--- flask/main.py
+++ flask/main.py
@@ -1,12 +1,18 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-   return 'Hello World!'
+   return str(datetime.now())
 
+
+def world():
+   return 'World!'
 
 if __name__ == '__main__':
+   app.add_url_rule('/world', 'world',world)
+   print(app.url_map)
    app.run(port=8080)
