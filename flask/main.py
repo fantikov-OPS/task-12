31631e89c6b762655547c89c5f9d3e21b3eac97c
Aliaksei <fanikov41@gmail.com>
2026-06-11 18:57:11 +0300

    add two_pow url

--- flask/main.py
+++ flask/main.py
@@ -5,14 +5,29 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def date_now():
    return str(datetime.now())
 
+@app.route('/hello/<name>')
+def hello_name(name):
+   return f"Hello {name}"
 
-def world():
-   return 'World!'
+@app.route('/blog/<int:postID>')
+def show_blog(postID):
+   return f'Blog Number {postID}'
+
+@app.route('/rev/<float:revNo>')
+def revisoin(revNo):
+   return f'Revision Number {revNo}'
+
+@app.route('/path_to/<path:myPath>')
+def my_path(myPath):
+   return f'My path {myPath}'
+
+@app.route('/two_pow/<int:twoNumber>')
+def two_number(twoNumber):
+   return str(2**twoNumber)
 
 if __name__ == '__main__':
-   app.add_url_rule('/world', 'world',world)
    print(app.url_map)
    app.run(port=8080)
